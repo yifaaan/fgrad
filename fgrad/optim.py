30da1ef78651bebad59a4a0e3b414313968b1d42
yifaaan <0219lyf@gmail.com>
2025-06-03 16:13:21 +0800

    add weight_decay for adam

--- fgrad/optim.py
+++ fgrad/optim.py
@@ -22,12 +22,13 @@ class SGD(Optimizer):
     
 
 class Adam(Optimizer):
-    def __init__(self, params, lr=0.001, b1=0.9, b2=0.999, eps=1e-8):
+    def __init__(self, params, lr=0.001, b1=0.9, b2=0.999, eps=1e-8, weight_decay=0., amsgrad=False):
         super().__init__(params)
         self.lr = lr
         self.b1 = b1
         self.b2 = b2
         self.eps = eps
+        self.wd, self.amsgrad = weight_decay, amsgrad
         self.m = [np.zeros_like(t.data) for t in self.params]
         self.v = [np.zeros_like(t.data) for t in self.params]
         self.t = 0
@@ -40,10 +41,13 @@ class Adam(Optimizer):
         for i, param in enumerate(self.params):
             if param.grad is None:
                 continue
-            self.m[i] = self.b1 * self.m[i] + (1 - self.b1) * param.grad
-            self.v[i] = self.b2 * self.v[i] + (1 - self.b2) * np.square(param.grad)
+            g = param.grad
+            self.m[i] = self.b1 * self.m[i] + (1 - self.b1) * g
+            self.v[i] = self.b2 * self.v[i] + (1 - self.b2) * np.square(g)
             m_hat = self.m[i] / bc1
             v_hat = self.v[i] / bc2
+            if self.wd:
+                param.data *= (1 - self.lr * self.wd)
             param.data -= self.lr * m_hat / (np.sqrt(v_hat) + self.eps)
 
         
