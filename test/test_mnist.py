fcbdf21f434dfa83fad6bb79acc9d7795219a2a0
yifaaan <0219lyf@gmail.com>
2025-06-04 12:59:51 +0800

    make test_mnist runnable by pytest and directly

--- test/test_mnist.py
+++ test/test_mnist.py
@@ -1,12 +1,17 @@
 # from nn import BobNet
+import unittest
 import numpy as np
 from tqdm import trange
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fgrad.utils import fetch_mnist, layer_init_uniform
 from fgrad.tensor import Tensor
-import fgrad.optim as optim
+import fgrad.optim as fgrad_optim
 
+
+X_train, Y_train, X_test, Y_test = fetch_mnist()
+X_train = X_train.astype(np.float32) / 255.0
+X_test = X_test.astype(np.float32) / 255.0
 
 class FBobNet:
   def __init__(self):
@@ -32,62 +37,63 @@ class FConvNet:
 
 
 
+class TestMNIST(unittest.TestCase):
+  def test_mnist(self):
+    if os.getenv("CONV") == "1":
+      model = FConvNet()
+      optim = fgrad_optim.Adam([model.c1, model.l1, model.l2], lr=0.001)
+      steps = 400
+    else:
+      model = FBobNet()
+      optim = fgrad_optim.Adam([model.l1, model.l2], lr=0.001)
+      steps = 1000
+
+    # training
+    BS = 128
+
+    losses, accuracies = [], []
+    for i in (t := trange(steps)):
+      samp = np.random.randint(0, X_train.shape[0], BS)
+      x = Tensor(X_train[samp].reshape(-1, 28*28))
+      Y = Y_train[samp]
+      # create one-hot matrix
+      y = np.zeros((BS, 10), dtype=np.float32)
+      # set true label to -1:one-hot
+      y[np.arange(BS), Y] = -10.0
+      y = Tensor(y)
+
+      # network
+      out = model.forward(x)
+
+      # NLL loss function
+      
+      loss = out.mul(y).mean()
+      loss.backward()
+      optim.step()
+      
+      cat = np.argmax(out.data, axis=1)
+      accuracy = (cat == Y).mean()
 
 
-X_train, Y_train, X_test, Y_test = fetch_mnist()
-X_train = X_train.astype(np.float32) / 255.0
-X_test = X_test.astype(np.float32) / 255.0
-
-if os.getenv("CONV") == "1":
-  model = FConvNet()
-  optim = optim.Adam([model.c1, model.l1, model.l2], lr=0.001)
-  steps = 400
-else:
-  model = FBobNet()
-  optim = optim.Adam([model.l1, model.l2], lr=0.001)
-  steps = 1000
+      # printing
+      loss = loss.data
+      losses.append(loss)
+      accuracies.append(accuracy)
+      t.set_description("loss %.2f, accuracy %.2f" % (loss, accuracy))
 
 
+    def numpy_eval():
+      Y_test_pred_out = model.forward(Tensor(X_test.reshape(-1, 28*28))) # [N, 10]
+      Y_test_pred = np.argmax(Y_test_pred_out.data, axis=1)
+      accuracy = (Y_test_pred == Y_test).mean()
+      return accuracy
+
+    print("test set accuracy is %f" % numpy_eval())
 
 
-# training
-BS = 128
-
-losses, accuracies = [], []
-for i in (t := trange(steps)):
-  samp = np.random.randint(0, X_train.shape[0], BS)
-  x = Tensor(X_train[samp].reshape(-1, 28*28))
-  Y = Y_train[samp]
-  # create one-hot matrix
-  y = np.zeros((BS, 10), dtype=np.float32)
-  # set true label to -1:one-hot
-  y[np.arange(BS), Y] = -10.0
-  y = Tensor(y)
-
-  # network
-  out = model.forward(x)
-
-  # NLL loss function
-  
-  loss = out.mul(y).mean()
-  loss.backward()
-  optim.step()
-  
-  cat = np.argmax(out.data, axis=1)
-  accuracy = (cat == Y).mean()
+if __name__ == "__main__":
+  unittest.main()
 
 
-  # printing
-  loss = loss.data
-  losses.append(loss)
-  accuracies.append(accuracy)
-  t.set_description("loss %.2f, accuracy %.2f" % (loss, accuracy))
 
 
-def numpy_eval():
-  Y_test_pred_out = model.forward(Tensor(X_test.reshape(-1, 28*28))) # [N, 10]
-  Y_test_pred = np.argmax(Y_test_pred_out.data, axis=1)
-  accuracy = (Y_test_pred == Y_test).mean()
-  return accuracy
-
-print("test set accuracy is %f" % numpy_eval())
